184503f6b7ab6083c7a8a47dfad45f6d4f2b2d1e
Zach Kurtz <zkurtz@protonmail.com>
2024-11-11 09:57:26 -0500

    fix version

--- debtcloset/__init__.py
+++ debtcloset/__init__.py
@@ -1,6 +1,8 @@
 """Initialize debtcloset package."""
 
+from importlib.metadata import version
 from pathlib import Path
 
+__version__ = version("dummio")
 SRC_DIR = Path(__file__).parent
 REPO_DIR = SRC_DIR.parent
